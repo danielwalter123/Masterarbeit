80900b2573f9a2a33f04d74b08343a2d92de2045
Udo Walter <udo.walter@mars.uni-freiburg.de>
2021-06-16 15:03:19 +0200

    add image diff ratio check

--- selenium_test/main.py
+++ selenium_test/main.py
@@ -2,7 +2,7 @@ import os
 import time
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-from PIL import Image, ImageDraw, ImageChops
+from PIL import Image, ImageDraw, ImageChops, ImageStat
 from io import BytesIO
 import numpy as np
 import easyocr
@@ -16,35 +16,51 @@ elem = driver.find_element_by_id("env")
 last_img = None
 last_scanned_img = None
 
+def image_diff(img1, img2):
+    if img1.mode != img2.mode or img1.size != img2.size or img1.getbands() != img2.getbands():
+        return 1.0
+    diff = ImageChops.difference(img1, img2)
+    if not diff.getbbox():
+        return 0.0
+    stat = ImageStat.Stat(diff)
+    diff_ratio = sum(stat.mean) / (len(stat.mean) * 255)
+    print(diff_ratio)
+    return diff_ratio
+
+PAUSE_TIME = 1
+
 while True:
     # Take a screenshot and crop it
     png = driver.get_screenshot_as_png()
     img = Image.open(BytesIO(png)).convert("RGB")
     img = img.crop((0, 0, elem.size["width"], elem.size["height"]))
+    if img.size[0] == 0 or img.size[1] == 0:
+        time.sleep(PAUSE_TIME)
+        continue
 
     # Check if the image is stable (no movement)
     if last_img:
-        diff = ImageChops.difference(last_img, img)
-        if diff.getbbox():
+        if image_diff(last_img, img) > 0.05:
             print("Image is not stable.")
             last_img = img.copy()
-            time.sleep(0.5)
+            time.sleep(PAUSE_TIME)
             continue
     else:
         last_img = img.copy()
+        time.sleep(PAUSE_TIME)
         continue
 
     # Read the text in the image
     if last_scanned_img:
-        diff = ImageChops.difference(last_scanned_img, img)
-        if not diff.getbbox():
+        if image_diff(last_scanned_img, img) == 0:
             print("Image has not changed since last scan.")
-            time.sleep(0.5)
+            time.sleep(PAUSE_TIME)
             continue
 
     last_scanned_img = img.copy()
     print("Scanning image.")
     result = reader.readtext(np.asarray(img), min_size=5, mag_ratio=2)
+    print("Scan complete.")
 
     for r in result:
         print(r[1])
@@ -62,5 +78,6 @@ while True:
             action.perform()
 
     #img.show()
+    time.sleep(PAUSE_TIME)
 
 driver.close()
--- selenium_test/stable_test.py
+++ selenium_test/stable_test.py
@@ -1,10 +1,10 @@
-from PIL import Image
-from PIL import ImageChops
+from PIL import Image, ImageChops, ImageStat
 
 img1 = Image.open("./screenshots/03.png").convert("RGB")
-img2 = Image.open("./screenshots/04.png").convert("RGB")
+img2 = Image.open("./screenshots/08.png").convert("RGB")
 
-diff = ImageChops.difference(img1, img1)
+diff = ImageChops.difference(img1, img2)
 
-diff.show()
-print(diff.getbbox())
+stat = ImageStat.Stat(diff)
+diff_ratio = sum(stat.mean) / (len(stat.mean) * 255)
+print(diff_ratio)
