0f85407a0f6c069d27eed27f881d87a12c268c6c
Udo Walter <udo.walter@mars.uni-freiburg.de>
2021-06-16 17:25:53 +0200

    add example yaml steps file

--- selenium_test/main.py
+++ selenium_test/main.py
@@ -1,20 +1,29 @@
 import os
 import time
+import yaml
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 from PIL import Image, ImageDraw, ImageChops, ImageStat
 from io import BytesIO
 import numpy as np
 import easyocr
 
 
+# Read yaml file
+steps = None
+with open("example.yaml", 'r') as stream:
+    try:
+        steps = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+if not steps or type(steps) is not list:
+    exit()
+
 reader = easyocr.Reader(['en'])
 driver = webdriver.Firefox()
 driver.get("file://" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "index.html"))
-
-elem = driver.find_element_by_id("env")
-last_img = None
-last_scanned_img = None
 
 def image_diff(img1, img2):
     if img1.mode != img2.mode or img1.size != img2.size or img1.getbands() != img2.getbands():
@@ -24,12 +33,39 @@ def image_diff(img1, img2):
         return 0.0
     stat = ImageStat.Stat(diff)
     diff_ratio = sum(stat.mean) / (len(stat.mean) * 255)
-    print(diff_ratio)
     return diff_ratio
 
-PAUSE_TIME = 1
 
-while True:
+OCR_FIX = {
+    "ok": ["ok", "tk", "0k"],
+    "file": ["file", "eile"]
+}
+
+PAUSE_TIME = 1
+elem = driver.find_element_by_id("env")
+last_img = None
+last_scanned_img = None
+
+step = steps.pop(0)
+while step:
+    print("Next step: " + str(step))
+
+    if type(step) is not list or len(step) < 2:
+        print("Invalid step!")
+        continue
+
+    if step[0] == "input":
+        time.sleep(3)
+        for char in step[1]:
+            action = ActionChains(driver)
+            if char.isupper():
+                action.key_down(Keys.SHIFT)
+            action.send_keys(char)
+            action.perform()
+            time.sleep(0.1)
+        step = steps.pop(0) if len(steps) > 0 else None
+        continue
+
     # Take a screenshot and crop it
     png = driver.get_screenshot_as_png()
     img = Image.open(BytesIO(png)).convert("RGB")
@@ -69,15 +105,20 @@ while True:
         bottom_right = (r[0][2][0], r[0][2][1])
         draw.rectangle((top_left, bottom_right), outline="red")
 
-        if r[1].lower() in ["ok", "0k", "tk"]:
+        ocr_text = r[1].lower()
+        step_text = step[1].lower()
+
+        if ocr_text == step_text or (step_text in OCR_FIX and ocr_text in OCR_FIX[step_text]):
             center = np.divide(np.add(top_left, bottom_right), 2)
             action = ActionChains(driver)
             action.reset_actions()
             action.move_to_element_with_offset(elem, center[0], center[1])
             action.click()
             action.perform()
+            step = steps.pop(0) if len(steps) > 0 else None
+            break
 
     #img.show()
     time.sleep(PAUSE_TIME)
 
-driver.close()
+#driver.close()
